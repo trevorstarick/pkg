58c535eddb089b7c0e968522816c2a314fa3b3e5
Trevor Starick <trevor.starick@gmail.com>
2026-03-01 22:47:48 -0500

    Generate prioritized template path list for requests
    Replace single-template resolution with a function that builds a prioritized list of candidate template paths based on the request URL, pattern, and Hx-Request header. Uses "ssr/" for Hx requests and "pages/" otherwise, walks up the path to include more general index pages, sorts candidates by specificity, deduplicates them, and prefixes each with configured TemplateDirs (plus "views"). Also adds a slices import and updates behavior to try multiple locations instead of returning a single path.

--- htmx/loader.go
+++ htmx/loader.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"slices"
 	"strings"
 )
 
@@ -36,26 +37,59 @@ func (htmx *HTMX) partialLoader(name string) (template.HTML, error) {
 	return template.HTML(strings.TrimSuffix(string(bytes), "\n")), nil
 }
 
+// convertRequestToTemplatePaths generates a list of potential template paths based on the request URL and pattern. It prioritizes more specific paths first, followed by more general ones, and finally includes a default index path.
+// For example, if the request URL is "/foo/bar" and the pattern is "/foo/*", it will generate paths like "ssr/foo/bar.html", "ssr/foo/index.html", "ssr/foo.html", and "ssr/index.html". The function also checks for the "Hx-Request" header to determine whether to use the "ssr/" or "pages/" directory for the templates.
+// The generated paths are sorted in descending order of specificity (i.e., paths with more segments are prioritized over those with fewer segments) to ensure that the most specific template is attempted first when rendering the response.
 func (htmx *HTMX) convertRequestToTemplatePaths(r *http.Request) []string {
 	var part string
 
 	if r.Header.Get("Hx-Request") == "true" {
-		part = "views/ssr/"
+		part = "ssr/"
 	} else {
-		part = "views/pages"
+		part = "pages"
 	}
 
-	pathName := "index"
+	path := r.URL.Path
+	paths := []string{}
+	for {
+		p := filepath.Join(part, strings.TrimSuffix(path[1:], "/"))
+		paths = append(paths, p+".html")
+		if filepath.Dir(p) != "/" {
+			paths = append(paths, filepath.Join(filepath.Dir(p), "index.html"))
+		} else {
+			break
+		}
 
-	if r.Pattern != "" && r.Pattern != "/" {
-		pathName = r.Pattern[1:]
-	} else if r.URL.Path != "/" {
-		pathName = r.URL.Path[1:]
+		path = filepath.Dir(path)
+
+		if path == "/" {
+			break
+		}
 	}
 
-	pathName = strings.TrimSuffix(pathName, "/")
+	paths = append(paths, filepath.Join(part, strings.TrimSuffix(r.Pattern[1:], "/")+".html"))
 
-	return filepath.Join(part, pathName+".html")
+	slices.SortFunc(paths, func(i, j string) int {
+		return strings.Count(j, "/") - strings.Count(i, "/")
+	})
+
+	paths = append(paths, filepath.Join(part, "index.html"))
+
+	uniquePathsMap := map[string]struct{}{}
+	uniquePaths := []string{}
+	for _, p := range paths {
+		if _, exists := uniquePathsMap[p]; !exists {
+			uniquePathsMap[p] = struct{}{}
+
+			for _, location := range append(htmx.TemplateDirs, []string{
+				"views",
+			}...) {
+				uniquePaths = append(uniquePaths, filepath.Join(location, p))
+			}
+		}
+	}
+
+	return uniquePaths
 }
 
 func readTemplateFile(path string) (string, error) {
